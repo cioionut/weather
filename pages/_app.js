4539fc93586a0b9de7401e91113ad4e93a9f93cb
Ionut Ciocoiu <ionutnciocoiu@gmail.com>
2020-11-13 19:03:48 +0200

    Add google analytics code

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globalStyles.css';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from '../lib/apolloClient'
 
 export default function App({ Component, pageProps }) {
-  const apolloClient = useApollo(pageProps.initialApolloState)
+  const apolloClient = useApollo(pageProps.initialApolloState);
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <ApolloProvider client={apolloClient}>
